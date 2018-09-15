97aa23d6e1d5c107b487c3cf162ce5c0b3e6d23a
AnthonyLam <alam6@stuy.edu>
2018-09-15 15:31:39 -0400

    finished work02

--- work02.c
+++ work02.c
@@ -5,5 +5,23 @@ char *ap = &a;
 
 int main(){
 	printf("Hex for int %x \n", a);
+	for (int i =0; i<4; i++){
+		printf("%hhx\n", *ap );
+		ap++;
+	}
+	ap=&a;
+	for (int i=0; i<4; i++){
+		*ap+=1;
+		ap+=1;
+		printf("%x\n", a);
+		printf("%d\n", a);
+	}
+	
+	for (int i=0; i<4; i++){
+		*ap+=16;
+		ap+=16;
+		printf("%x\n", a);
+		printf("%d\n", a);
+	}
 	return 0;
 }
